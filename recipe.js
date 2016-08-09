b82905e9d4b0127755f8d1749c647c7f7935724e
JENNIFER YAMADA <jenny.yamada@gmail.com>
2016-08-09 11:32:47 -0700

    done

--- recipe.js
+++ recipe.js
@@ -1,1 +1,23 @@
 'use strict';
+var recipes = {};
+
+function addIngredient(recipes, ingredients, amounts) {
+  recipes[ingredients] = amounts;
+  return recipes;
+}
+
+function removeIngredient(recipe, ingredient) {
+  delete recipe[ingredient];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredient, amount) {
+  recipe[ingredient] = amount;
+  return recipe;
+}
+
+function readRecipe(recipe) {
+  for ( var key in recipe ){
+    console.log(`this recipe calls for ${recipe[key]} of ${key}`);
+  }
+}
